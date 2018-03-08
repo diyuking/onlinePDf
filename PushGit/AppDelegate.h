b47bb9ab1ea2cb5d39523c63dda3c39b69700974
diyuking <diyuking@126.com>
2018-03-08 15:10:43 +0800

    上传代码测试

---
+++ PushGit/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  PushGit
+//
+//  Created by k on 2018/3/8.
+//  Copyright © 2018年 poor kid. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ PushGit/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  PushGit
+//
+//  Created by k on 2018/3/8.
+//  Copyright © 2018年 poor kid. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
